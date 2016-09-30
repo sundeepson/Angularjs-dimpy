2f84f683026a4ed134c76f964316197733c2972e
Maddela Dinakara sandeep <sundeepson@gmail.com>
2016-09-30 08:37:50 +0530

    Angular dimpy

---
+++ WebContent/notes-show-controller.js
@@ -0,0 +1,11 @@
+angular.module('NoteWrangler')
+.controller('NotesShowController', [function() {
+
+}]);
+angular.module('NoteWrangler').controller('NotesShowController', ['$routeParams', '$http', function($routeParams, $http) {
+  var controller = this;
+
+  $http({method: 'GET', url: '/notes/' + $routeParams.id}).success(function(data){
+    controller.note = data;
+  });
+}]);
